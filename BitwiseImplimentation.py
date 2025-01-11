63d3d5a021034f8ca449903542d8077cb60e60a9
MeghanaCH09 <meghanacheruvu@gmial.com>
2025-01-11 13:06:48 +0200

    BitwiseImplimentation

---
+++ BitwiseImplimentation.py
@@ -0,0 +1,10 @@
+num1 = 32
+num2 = 3
+
+print("num1 & num2 are: ", num1, num2)
+print("num1 | num2 are: ", num1 | num2)
+print("num1 ^ num2 are: ", num1 ^ num2)
+print("num1 << num2 are: ", num1 << num2)
+print("num1 >> num2 are: ", num1 >> num2)
+print("~num1 is: ", ~num1)
+print("~num2 is: ", ~num2)
